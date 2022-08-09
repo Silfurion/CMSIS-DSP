1053f6d725e4b38aa1d010471411d219f807dabc
Silfurion <nathan@lesbottet.net>
2022-08-09 15:22:22 +0200

    Update arm_f64_to_float.c

--- Source/SupportFunctions/arm_f64_to_float.c
+++ Source/SupportFunctions/arm_f64_to_float.c
@@ -1,8 +1,30 @@
-//
-//  arm_f64_to_float.cpp
-//  CMSISDSPSupport
-//
-//  Created by NBottet on 04/08/2022.
-//
 
-#include "arm_f64_to_float.hpp"
+
+#include "dsp/support_functions.h"
+
+
+
+
+void arm_f64_to_float(
+  const float64_t * pSrc,
+        float32_t * pDst,
+        uint32_t blockSize)
+{
+    const float64_t *pIn = pSrc;      /* Src pointer */
+    uint32_t  blkCnt;           /* loop counter */
+
+    /*
+     * Loop over blockSize number of values
+     */
+    blkCnt = blockSize;
+
+    while (blkCnt > 0U)
+    {
+
+        *pDst++ = (float32_t) * pIn++;
+        /*
+         * Decrement the loop counter
+         */
+        blkCnt--;
+    }
+}
